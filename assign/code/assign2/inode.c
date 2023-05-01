da7512af5549961aa6f93cc4dd072e1c13832910
Hung Vu <vungochung96@gmail.com>
2023-05-02 02:17:18 +0700

    restore starter code for assignment2

--- assign/code/assign2/inode.c
+++ assign/code/assign2/inode.c
@@ -12,38 +12,13 @@
 #define TOTAL_BLOCKS_FROM_INDIR (BLOCKS_PER_INDIR * NUM_INDIR_BLOCKS)
 
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
-  int offset = (inumber-1) / INODE_PER_BLOCK;
-  struct inode buf[INODE_PER_BLOCK];
-  int bytesRead = diskimg_readsector(fs->dfd, INODE_START_SECTOR + offset, buf);
-  if (bytesRead != DISKIMG_SECTOR_SIZE) {
-    fprintf(stderr, "Error reading inode %d\n", inumber);
-    return -1;
-  }
-  *inp = buf[(inumber-1) % INODE_PER_BLOCK];
-  return 0;
+
 }
 
 int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {
-  if (!(inp->i_mode & ILARG)) {
-    // not a large file
-    return inp->i_addr[blockNum];
-  }
-  uint16_t buf[BLOCKS_PER_INDIR];
-  if (blockNum < TOTAL_BLOCKS_FROM_INDIR) {
-    // singly indirect
-    // each block 256 * 2-byte block numbers
-    int indirBlockNum = blockNum / BLOCKS_PER_INDIR;
-    diskimg_readsector(fs->dfd, inp->i_addr[indirBlockNum], buf);
-    return buf[blockNum % BLOCKS_PER_INDIR];
-  } else {
-    // doubly indirect
-    int indirBlockNum = (blockNum - TOTAL_BLOCKS_FROM_INDIR) / BLOCKS_PER_INDIR;
-    diskimg_readsector(fs->dfd, inp->i_addr[7], buf);
-    diskimg_readsector(fs->dfd, buf[indirBlockNum], buf);
-    return buf[blockNum % BLOCKS_PER_INDIR];
-  }
+
 }
 
 int inode_getsize(struct inode *inp) {
-  return ((inp->i_size0 << 16) | inp->i_size1); 
+
 }
--- assign/code/assign2/pathname.c
+++ assign/code/assign2/pathname.c
@@ -10,21 +10,10 @@
 #define PATH_SEP "/"
 
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
-  if (strcmp(pathname, PATH_SEP) == 0) return ROOT_INUMBER;
-  struct direntv6 dirEnt;
-  char pathname_cpy[strlen(pathname)+1];
-  strcpy(pathname_cpy, pathname);
-  char *tok = strtok(pathname_cpy + 1, PATH_SEP);
-  return pathname_lookup_helper(fs, ROOT_INUMBER, tok, &dirEnt);
+ 
 }
 
 int pathname_lookup_helper(struct unixfilesystem *fs, const int dirinumber,
                            char *tok, struct direntv6 *dirEnt) {
-  if (directory_findname(fs, tok, dirinumber, dirEnt) < 0) {
-    return -1;
-  }
-  tok = strtok(NULL, PATH_SEP);
-  return (tok == NULL)
-         ? dirEnt->d_inumber
-         : pathname_lookup_helper(fs, dirEnt->d_inumber, tok, dirEnt);
+ 
 }
