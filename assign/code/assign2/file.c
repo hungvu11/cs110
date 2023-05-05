e961593bb6803e682f9afc63e0bc2c3a33a7019a
Hung Vu <vungochung96@gmail.com>
2023-05-05 21:04:24 +0700

    assign2: finish file.c

--- assign/code/assign2/file.c
+++ assign/code/assign2/file.c
@@ -6,5 +6,17 @@
 #include "diskimg.h"
 
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
+    struct inode inp;
+    if (inode_iget(fs, inumber, &inp) < 0) return -1;
+    int block_add;
+    if ((block_add = inode_indexlookup(fs, &inp, blockNum)) < 0) return -1;
+    if (diskimg_readsector(fs->dfd, block_add, buf) < 0) return -1;
 
+    int size = inode_getsize(&inp);
+    int total_block = size / DISKIMG_SECTOR_SIZE;
+    if (blockNum == total_block) {
+        return size % DISKIMG_SECTOR_SIZE;
+    } else {
+        return DISKIMG_SECTOR_SIZE;
+    }
 }
