32c9afdee3d2f4ca79ad69cfe19b7ee2eb11645e
Vu Ngoc Hung <hung1.vu@lge.com>
2023-05-16 18:28:37 +0700

    assign3: unfinished implementation for subprocess.cc

--- assign/code/assign3/subprocess.cc
+++ assign/code/assign3/subprocess.cc
@@ -9,5 +9,17 @@
 using namespace std;
 
 subprocess_t subprocess(char *argv[], bool supplyChildInput, bool ingestChildOutput) throw (SubprocessException) {
-
+    int fds[2];
+    pipe(fds);
+    subprocess_t process = { fork(), fds[1], fds[0] };
+    if (process.pid == 0) { 
+        close(fds[1]); // no writing necessary
+        if (supplyChildInput == true) {
+            dup2(fds[0], STDIN_FILENO);
+            close(fds[0]); // already duplicated
+        }
+        execvp(argv[0], argv);
+    }
+    close(fds[0]); // not used in parent
+    return process;
 }
