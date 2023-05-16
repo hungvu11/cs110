d8f1b6e02ffddda9ffdadbcd3c26e443d7c9ecc9
Vu Ngoc Hung <hung1.vu@lge.com>
2023-05-16 14:11:50 +0700

    implement for pipeline.c

--- assign/code/assign3/pipeline.c
+++ assign/code/assign3/pipeline.c
@@ -10,5 +10,27 @@
 #include <errno.h>
 
 void pipeline(char *argv1[], char *argv2[], pid_t pids[]) {
+    int fds[2];
+    pipe(fds);
+    pid_t read = fork();
+    if (read == 0) {
+        // child
+        close(fds[1]); // no need to write
+        dup2(fds[0], STDIN_FILENO);
+        close(fds[0]);
+        execvp(argv2[0], argv2);
+    }
+    pids[1] = read;
 
+    pid_t write = fork();
+    if (write == 0) {
+        // child
+        close(fds[0]);
+        dup2(fds[1], STDOUT_FILENO);
+        close(fds[1]);
+        execvp(argv1[0], argv1);
+    }
+    pids[0] = write;
+    close(fds[0]);
+    close(fds[1]);
 }
