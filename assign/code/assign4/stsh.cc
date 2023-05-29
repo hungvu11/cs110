5031d032183ba63f477ecc5dda3db6176fc48753
Vu Ngoc Hung <hung1.vu@lge.com>
2023-05-29 16:17:44 +0700

    implement the SIGCHLD handler

--- assign/code/assign4/stsh.cc
+++ assign/code/assign4/stsh.cc
@@ -22,7 +22,7 @@
 using namespace std;
 
 static STSHJobList joblist; // the one piece of global data we need so signal handlers can access it
-
+static pid_t fgpid = 0;
 /**
  * Function: handleBuiltin
  * -----------------------
@@ -48,6 +48,39 @@ static bool handleBuiltin(const pipeline& pipeline) {
   return true;
 }
 
+static void toggleSIGCHLDBlock(int how) {
+  sigset_t mask;
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGCHLD);
+  sigprocmask(how, &mask, NULL);
+}
+
+void blockSIGCHLD() {
+  toggleSIGCHLDBlock(SIG_BLOCK);
+}
+
+void unblockSIGCHLD() {
+  toggleSIGCHLDBlock(SIG_UNBLOCK);
+}
+
+static void waitForForegroundProcess(pid_t pid) {
+  fgpid = pid;
+  sigset_t empty;
+  sigemptyset(&empty);
+  while (fgpid == pid) {
+    sigsuspend(&empty);
+  }
+  unblockSIGCHLD();
+}
+
+static void reapChild(int sig) {
+  while (true) {
+    pid_t pid = waitpid(-1, NULL, WNOHANG);
+    if (pid <= 0) break;
+    if (pid == fgpid) fgpid = 0; // clear foreground process
+  }
+}
+
 /**
  * Function: installSignalHandlers
  * -------------------------------
@@ -60,6 +93,7 @@ static void installSignalHandlers() {
   installSignalHandler(SIGQUIT, [](int sig) { exit(0); });
   installSignalHandler(SIGTTIN, SIG_IGN);
   installSignalHandler(SIGTTOU, SIG_IGN);
+  installSignalHandler(SIGCHLD, reapChild);
 }
 
 /**
@@ -68,9 +102,10 @@ static void installSignalHandlers() {
  * Creates a new job on behalf of the provided pipeline.
  */
 static void createJob(const pipeline& p) {
-  
+  blockSIGCHLD();
   pid_t pid = fork();
   if (pid == 0) { //child 
+    unblockSIGCHLD();
     command cmd = p.commands[0];
     char* argv[kMaxArguments + 2];
     argv[0] = cmd.command;
@@ -83,8 +118,7 @@ static void createJob(const pipeline& p) {
   job.addProcess(STSHProcess(pid, p.commands[0]));
   cout << joblist;
   setpgid(pid, 0);
-  waitpid(pid, NULL, 0);
-
+  waitForForegroundProcess(pid);
   STSHProcess& process = job.getProcess(pid);
   process.setState(kTerminated);
   joblist.synchronize(job);
