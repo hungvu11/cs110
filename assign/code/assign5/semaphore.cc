d4a49b9b406a4a5dc361ce165160eaaf25c7e001
Vu Ngoc Hung <hung1.vu@lge.com>
2023-06-02 10:05:30 +0700

    assign5: restore starting code

--- assign/code/assign5/semaphore.cc
+++ assign/code/assign5/semaphore.cc
@@ -2,9 +2,6 @@
 // Created by Edmund Mok on 8/7/18.
 //
 
-#include <iostream>
-#include "ostreamlock.h"
-#include <boost/thread/thread.hpp>
 #include "semaphore.h"
 
 using namespace std;
@@ -18,11 +15,5 @@ void semaphore::wait() {
 void semaphore::signal() {
   lock_guard<mutex> lg(m);
   value++;
-  if (value == 1) cv.notify_all(); // cv.notify_one();
-}
-
-void semaphore::signal(signal_condition) {
-  boost::this_thread::at_thread_exit([this] {
-      this->signal();
-  });
+  if (value == 1) cv.notify_one(); // cv.notify_all();
 }
